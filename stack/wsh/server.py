5b26d3479963f79b9ff368ae80def6fba1b6d4b7
Ryan.K <ryankung@ieee.org>
2016-06-08 14:24:43 +0800

    output stdio to ws

--- stack/wsh/server.py
+++ stack/wsh/server.py
@@ -5,13 +5,15 @@ from aiohttp import web
 from typing import Callable
 from functools import partial
 from ..utils import io_echo
+from io import StringIO
+import sys
 
 
 def out(s: str):
     '''
     Show info message with yellow color
     '''
-    return io_echo("\033[92mOUT: {}\033[00m" .format(s))
+    return io_echo("\033[92mOut: {}\033[00m" .format(s))
 
 
 def command_parser(cmd: str, fns: dict) -> Callable:
@@ -25,7 +27,19 @@ def command_parser(cmd: str, fns: dict) -> Callable:
     fn_name = args[0]
     args = [i for i in args[1:] if not i.startswith('-')]
     kwargs = dict([i.replace('-', '').split('=') for i in args[1:] if i.startswith('-')])
-    return partial(fns.get(fn_name, lambda: 'None'), *args, **kwargs)()
+    return partial(fns.get(fn_name, lambda: 'None'), *args, **kwargs)
+
+
+def io_wrapper(fn: Callable, ws: web.WebSocketResponse) -> str:
+    io = StringIO()
+    sys.stdout = sys.stderr = io
+    res = fn() or io.getvalue()
+    sys.stdout = sys.__stdout__
+    sys.stderr = sys.__stderr__
+    ws.send_str(out(res))
+    io.close()
+    del io
+    return res
 
 
 async def wsh(request, handler=print):
@@ -33,8 +47,7 @@ async def wsh(request, handler=print):
     await ws.prepare(request)
     async for msg in ws:
         if msg.tp == web.MsgType.text:
-            res = handler(msg.data)
-            ws.send_str(out(res))
+            io_wrapper(handler(msg.data), ws)
         elif msg.tp == web.MsgType.binary:
             ws.send_bytes(msg.data)
         elif msg.tp == web.MsgType.close:
